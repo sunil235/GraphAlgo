130f4a05d5ae1952e5474d10134caafb7ddfb47f
sunil.dabbiru <35950438+sunil235@users.noreply.github.com>
2022-04-30 18:29:52 -0400

    Updated DFS  and BFS  methods

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # Depth First Search
-from collections import deque
 
+from timeit import default_timer as timer
+from datetime import timedelta
 
+"""
 def deptFirstPrint(graph: dict, source):
     stack = [source]
     while len(stack) > 0:
@@ -10,6 +12,18 @@ def deptFirstPrint(graph: dict, source):
         for neighbour in graph[current]:
             stack.append(neighbour)
 
+"""
+
+
+def deptFirstPrint(graph, node, visited):
+    if node not in visited:
+        visited.append(node)
+        for k in graph[node]:
+            deptFirstPrint(graph, k, visited)
+    return visited
+
+
+# Breadth First Search
 
 def breadthFirstPrint(graph: dict, source):
     queue = [source]
@@ -26,8 +40,26 @@ if __name__ == '__main__':
     z = input("Enter the search DFS/BFS:")
 
     list1 = {'a': ['b', 'c'], 'b': ['d'], 'c': ['e'], 'd': ['f'], 'e': [], 'f': []}
+    """
+    list1 = {
+        'A': ['B', 'S'],
+        'B': ['A'],
+        'C': ['D', 'E', 'F', 'S'],
+        'D': ['C'],
+        'E': ['C', 'H'],
+        'F': ['C', 'G'],
+        'G': ['F', 'S'],
+        'H': ['E', 'G'],
+        'S': ['A', 'C', 'G']
+    }
+    """
     if z == 'DFS':
-        deptFirstPrint(list1, 'a')
+        start = timer()
+        print(deptFirstPrint(list1, 'a', []))
+        end = timer()
+        print("\nTime taken for " + z + " is :" + str(timedelta(seconds=end - start)) + " seconds")
     else:
-        pass
-        breadthFirstPrint(list1, 'a')
+        start = timer()
+        breadthFirstPrint(list1, 'A')
+        end = timer()
+        print("\nTime taken for " + z + " is :" + str(timedelta(seconds=end - start)) + " seconds")
