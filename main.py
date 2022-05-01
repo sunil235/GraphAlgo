cd11f338ead67da6f2ece23937b493e23d25111e
sunil.dabbiru <35950438+sunil235@users.noreply.github.com>
2022-05-01 17:19:35 -0400

    Updated DFS  and BFS  methods

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def deptFirstPrint(graph: dict, source):
 """
 
 
-def deptFirstPrint(graph, node, visited):
+def deptFirstPrint(graph: dict, node, visited):
     if node not in visited:
         visited.append(node)
         for k in graph[node]:
@@ -25,22 +25,24 @@ def deptFirstPrint(graph, node, visited):
 
 # Breadth First Search
 
-def breadthFirstPrint(graph: dict, source):
-    queue = [source]
-    while len(queue) > 0:
-        current = queue[0]
-        print(current, end=" ")
-        queue = queue[1:]
-        for neighbour in graph[current]:
-            queue.append(neighbour)
+def breadthFirstPrint(graph: dict, node, visited):
+    if node not in visited:
+        visited.append(node)
+    while len(visited) > 0:
+        current = graph[node]
+        #graph = graph[1:]
+        for k in graph[current]:
+            if k not in visited:
+                visited.append(k)
+    return visited
 
 
 if __name__ == '__main__':
 
     z = input("Enter the search DFS/BFS:")
 
-    list1 = {'a': ['b', 'c'], 'b': ['d'], 'c': ['e'], 'd': ['f'], 'e': [], 'f': []}
-    """
+    # list1 = {'a': ['b', 'c'], 'b': ['d'], 'c': ['e'], 'd': ['f'], 'e': [], 'f': []}
+
     list1 = {
         'A': ['B', 'S'],
         'B': ['A'],
@@ -52,14 +54,14 @@ if __name__ == '__main__':
         'H': ['E', 'G'],
         'S': ['A', 'C', 'G']
     }
-    """
+
     if z == 'DFS':
         start = timer()
-        print(deptFirstPrint(list1, 'a', []))
+        print(deptFirstPrint(list1, 'F', []))
         end = timer()
         print("\nTime taken for " + z + " is :" + str(timedelta(seconds=end - start)) + " seconds")
     else:
         start = timer()
-        breadthFirstPrint(list1, 'A')
+        print(breadthFirstPrint(list1, 'F', []))
         end = timer()
         print("\nTime taken for " + z + " is :" + str(timedelta(seconds=end - start)) + " seconds")
