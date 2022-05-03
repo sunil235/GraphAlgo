7e3501211845b61fc89e9b24750bbf9616c3adb3
sunil.dabbiru <35950438+sunil235@users.noreply.github.com>
2022-05-03 00:57:25 -0400

    Added req file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='GraphAlgo',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Sunil',
+    author_email='',
+    description=''
+)
